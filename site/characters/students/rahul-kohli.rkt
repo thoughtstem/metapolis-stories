dfb7b87b990cf89a243931c69599aaeff26610df
Sara <sara@thoughtstem.com>
2019-12-02 17:04:24 -0800

    ch5 complete content

---
+++ site/characters/students/rahul-kohli.rkt
@@ -0,0 +1,7 @@
+#lang at-exp metapolis-stories/lang/character
+
+(define name "Rahul Kohli")
+
+@paras{
+  Rahul is a 4th grade student who attends Turing Recreation Center's MetaCoders Spring Session class. This is his first class with MetaCoders, though he has done some coding at home before.
+}
---
+++ site/characters/students/skylar-thompson.rkt
@@ -0,0 +1,7 @@
+#lang at-exp metapolis-stories/lang/character
+
+(define name "Skylar Thompson")
+
+@paras{
+  Skylar is a 3rd grade student who attends Turing Recreation Center's MetaCoders Spring Session class. This is her first class with MetaCoders, though she has done some coding in her school before.
+}
